108717f38ce375a85cd09125af9eadf7df9fc8b3
Olivia Wang <Olivia@Olivias-MacBook-Pro.local>
2015-02-03 11:16:51 -0800

    Initial Commit

---
+++ lab1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  lab1
+//
+//  Created by Olivia Wang on 2/3/15.
+//  Copyright (c) 2015 Olivia Wang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
